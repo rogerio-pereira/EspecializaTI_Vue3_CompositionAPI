e71bf71886b5cb0b5150b3f1277608d1d1fac5d3
Rogerio Pereira <hi@rogeriopereira.dev>
2023-10-30 19:47:22 -0400

    Instalar/configurar axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import { URL_API } from '@/configs/api'
+
+axios.defaults.baseURL = URL_API
 
 const app = createApp(App)
 
