3cade54803a1ac8c55c864e1f7893e579e17eef7
Rogerio Pereira <hi@rogeriopereira.dev>
2023-10-30 19:06:34 -0400

    ENTENDENDO UM COMPONENTE NO VUE 3

--- frontend/src/views/Todo/TodoView.vue
+++ frontend/src/views/Todo/TodoView.vue
@@ -1,5 +1,38 @@
 <template>
     <main>
         <h1>ToDo</h1>
+
+        <ul>
+            <li v-for='todo in todos' :key='todo.id'>
+                {{ todo.name }}
+            </li>
+        </ul>
     </main>
+
+    <input type='text' v-model='name' />
 </template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+    name: 'Todos',
+
+    setup() {
+        const todos = [
+            {id: 1, name: 'tarefa 1', completed: true },
+            {id: 2, name: 'tarefa 2', completed: true },
+            {id: 3, name: 'tarefa 3', completed: true },
+        ]
+        const name = ref('test') // Variavel precisa ser definida com ref para funcionar o two-way data-bind
+
+        // change name value
+        // name.value = 'test ref'
+
+        return {
+            todos,
+            name
+        }
+    }
+}
+</script>
