4945e30d384a4147decaee8d070ccc3ca4467d41
Rogerio Pereira <hi@rogeriopereira.dev>
2023-10-30 19:47:30 -0400

    CONSUMIR API REST

---
+++ frontend/src/services/todo.service.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+export default class TodoService {
+    static async getAll() {
+        return axios.get('/todos')
+    }
+}
--- frontend/src/views/Todo/TodoView.vue
+++ frontend/src/views/Todo/TodoView.vue
@@ -4,7 +4,7 @@
 
         <ul>
             <li v-for='todo in todos' :key='todo.id'>
-                {{ todo.name }}
+                {{ todo.title }}
             </li>
         </ul>
     </main>
@@ -13,25 +13,42 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import TodoService from '@/services/todo.service'
 
 export default {
     name: 'Todos',
 
     setup() {
-        const todos = [
-            {id: 1, name: 'tarefa 1', completed: true },
-            {id: 2, name: 'tarefa 2', completed: true },
-            {id: 3, name: 'tarefa 3', completed: true },
-        ]
-        const name = ref('test') // Variavel precisa ser definida com ref para funcionar o two-way data-bind
+        // const todos = [
+        //     {id: 1, name: 'tarefa 1', completed: true },
+        //     {id: 2, name: 'tarefa 2', completed: true },
+        //     {id: 3, name: 'tarefa 3', completed: true },
+        // ]
+        // const name = ref('test') // Variavel precisa ser definida com ref para funcionar o two-way data-bind
 
         // change name value
         // name.value = 'test ref'
 
+        // return {
+        //     todos,
+        //     name
+        // }
+
+        const todos = ref([])
+
+        onMounted(() => {
+            TodoService.getAll()
+                .then(response => {
+                    todos.value = response.data.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        })
+
         return {
             todos,
-            name
         }
     }
 }
